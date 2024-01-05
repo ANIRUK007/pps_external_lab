1661e9b7b920eafc1f0c778230c97edf350b2745
Anirudh Kotla <kotlaanirudh05@gmail.com>
2024-01-05 03:00:30 -0800

    1A

---
+++ 1A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int num;
+    printf("Enter any number:");
+    scanf("%d",&num);
+    if(num%2==0)
+    {
+        printf("Its an even number!!");
+    }
+    else{
+        printf("Its an odd number!!!!");
+    }
+    }
