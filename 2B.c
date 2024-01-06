90f2808f3c719f4c454bbf7ef2cc5d878fd69cf3
Anirudh Kotla <kotlaanirudh05@gmail.com>
2024-01-06 04:56:32 -0800

    2B

---
+++ 2B.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int arr[50],i,j,temp,size;
+    printf("Enter the Size of Array:");
+    scanf("%d",&size);
+    printf("Enter the element Of array");
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<size-1;i++)
+    {
+        for(j=i+1;i<=size-1;j++)
+        {
+            if(arr[j]<arr[i])
+            {
+                temp=arr[j];
+                arr[j]=temp;
+            }
+        }
+    }
+    printf("Elements after selection sorting:");
+    for(i=0;i<size;i++)
+    {
+    printf("%d",arr[i]);
+    }
+}
